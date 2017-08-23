98d7e2c6635cd972e510a8e721d8b5ef9c2342e3
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-23 16:37:54 -0600

    another option

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 3000)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,DELETE,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
