45ca60e4c9883995c59a3d4ca94e9efaf4fd290d
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-23 16:34:06 -0600

    trying new header config

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ const WelcomeController = require('./lib/controllers/welcome_controller.js')
 app.set('port', process.env.PORT || 3000)
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
     next();
 });
 
