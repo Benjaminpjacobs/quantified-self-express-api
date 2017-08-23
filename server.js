31b0629b9d182c0b6d8dcea31d496fb851d42cd3
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-23 16:32:29 -0600

    fix headers order

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", 'GET,POST,PUT,DELETE')
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
