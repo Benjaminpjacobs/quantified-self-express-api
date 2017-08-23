92933c2585a593f9f477459126c8cefdb0b23bfa
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-23 08:30:38 -0600

    alter seed files for prod

--- db/seeds/production/foods.js
+++ db/seeds/production/foods.js
@@ -1,49 +1,49 @@
 exports.seed = function(knex, Promise) {
-    return knex.raw('TRUNCATE foods RESTART IDENTITY')
-        .then(function() {
-            return Promise.all([
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Banana", 150, 1, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Yogurt", 200, 1, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Granola", 300, 1, new Date]
-                ),
-            ])
-        }).then(function() {
-            return Promise.all([
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Turkey Sandwhich", 450, 2, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Potato Chips", 200, 2, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Apple", 100, 2, new Date]
-                ),
-            ])
-        }).then(function() {
-            return Promise.all([
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Carrot Sticks", 150, 3, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Grapes", 100, 3, new Date]
-                ),
-            ])
-        }).then(function() {
-            return Promise.all([
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Chicken Breast", 400, 4, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Green Salad", 200, 4, new Date]
-                ),
-                knex.raw(
-                    'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Chocolate Cake", 500, 4, new Date]
-                )
-            ])
-        })
+    return knex.raw('SELECT * FROM foods')
+        // .then(function() {
+        //     return Promise.all([
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Banana", 150, 1, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Yogurt", 200, 1, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Granola", 300, 1, new Date]
+        //         ),
+        //     ])
+        // }).then(function() {
+        //     return Promise.all([
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Turkey Sandwhich", 450, 2, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Potato Chips", 200, 2, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Apple", 100, 2, new Date]
+        //         ),
+        //     ])
+        // }).then(function() {
+        //     return Promise.all([
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Carrot Sticks", 150, 3, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Grapes", 100, 3, new Date]
+        //         ),
+        //     ])
+        // }).then(function() {
+        //     return Promise.all([
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Chicken Breast", 400, 4, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Green Salad", 200, 4, new Date]
+        //         ),
+        //         knex.raw(
+        //             'INSERT INTO foods (name, calories, meal_id, created_at) VALUES (?, ?, ?, ?)', ["Chocolate Cake", 500, 4, new Date]
+        //         )
+        //     ])
+        // })
 };
--- db/seeds/production/meals.js
+++ db/seeds/production/meals.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
-    return knex.raw('SELECT * FROM meals')
+    return knex.raw('TRUNCATE meals RESTART IDENTITY')
         .then(function() {
             return knex.raw(
                 'INSERT INTO meals (name, created_at) VALUES (?, ?)', ["Breakfast", new Date]
